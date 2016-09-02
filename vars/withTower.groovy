8d7bac39f9912f92ab13545e2bbe43a815b16487
Patrick Wolf <patrick.wolf+1@gmail.com>
2016-09-01 20:48:38 -0700

    simplify for test

--- vars/withTower.groovy
+++ vars/withTower.groovy
@@ -1,13 +1,15 @@
 import java.util.Random
 
-def call(String host, String credential, Closure body){
+def call (Closure body){
     host = "foo.com"
 
     number = "555555"  //getRandom()
     workingDir = "$env.WORKSPACE/$number"
 
     dir (workingDir) {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "tower-cli", passwordVariable: '$password', usernameVariable: '$username']]) {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding',
+                          credentialsId: "tower-cli", passwordVariable:
+                                  '$password', usernameVariable: '$username']]) {
             writeFile file: './.tower_cli.cfg', text: "host: $host \n username: $username \n password $password"
         }
 
