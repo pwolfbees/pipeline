28b45b79d94690948c5269fd84a8816637d06cd0
Patrick Wolf <patrick.wolf+1@gmail.com>
2016-09-01 22:46:05 -0700

    simplify for test

--- vars/withTower.groovy
+++ vars/withTower.groovy
@@ -2,13 +2,14 @@ import org.apache.commons.lang.RandomStringUtils
 
 def call (host,credentials,Closure body){
 
-    workingDir = "tmp/${RandomStringUtils.random(9, true, true)}"
+    dir ("tmp/${RandomStringUtils.random(9, true, true)}") {
 
-    dir (workingDir) {
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "$credentials",
                           usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+
             writeFile file: './.tower_cli.cfg',
                     text: "host: $host\nusername: $env.USERNAME \npassword: $env.PASSWORD"
+
             body()
         }
 
