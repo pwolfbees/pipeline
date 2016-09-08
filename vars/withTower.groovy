ee4747601dfe6f660da82140bd7ffc8df888c278
patrickdummy <patrick.wolf+1@gmail.com>
2016-09-07 20:07:35 -0700

    Update withTower.groovy
    Updated script to remove Dir for now since it is not working correctly with Docker.inside.

--- vars/withTower.groovy
+++ vars/withTower.groovy
@@ -1,13 +1,10 @@
-import org.apache.commons.lang.RandomStringUtils
 
-def call (host, credentials, Closure body){
-    dir ("tmp/${RandomStringUtils.random(9, true, true)}") {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "$credentials",
-                          usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            writeFile file: './.tower_cli.cfg',
-                    text: "host: $host\nusername: $env.USERNAME \npassword: $env.PASSWORD"
-            body()
+def call (Map params, body){
+    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "$params.credentials",
+                    usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+        writeFile file: './.tower_cli.cfg',
+                text: "host: $params.host\nusername: $env.USERNAME \npassword: $env.PASSWORD"
+        body()
         }
-        deleteDir()
-    }
+        sh "rm ./.tower_cli.cfg"
 }
