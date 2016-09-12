77c8f0c44b7ceec5033345a2b117dc60e0dad8c9
pwolfbees <pwolf@beescloud.com>
2016-09-12 16:52:00 -0700

    Update withTower.groovy
    ignoring ssl warnings

--- vars/withTower.groovy
+++ vars/withTower.groovy
@@ -3,7 +3,7 @@ def call (Map params, body){
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "$params.credentials",
                     usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
         writeFile file: './.tower_cli.cfg',
-                text: "host: $params.host\nusername: $env.USERNAME \npassword: $env.PASSWORD"
+                text: "host: $params.host\nusername: $env.USERNAME \npassword: $env.PASSWORD\nverify_ssl: false"
         body()
         }
         sh "rm ./.tower_cli.cfg"
