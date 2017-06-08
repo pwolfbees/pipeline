cdbd6238a4db58ffd19126c107d97ce616f33949
pwolfbees <pwolf@beescloud.com>
2017-06-08 12:05:53 -0700

    Create purge.groovy

--- vars/purge.groovy
+++ vars/purge.groovy
@@ -1,10 +1,12 @@
 import hudson.model.Result;
 
-executor = currentBuild.rawBuild.getParent().getBuildByNumber(currentBuild.number-1).getExecutor()
+def call() {
+    executor = currentBuild.rawBuild.getParent().getBuildByNumber(currentBuild.number-1).getExecutor()
 
-if (executor != null) {
-    executor.interrupt(Result.NOT_BUILT)
-    executor = null
-} else {
-    echo "could not cancel an older flow because it has no assigned executor"
+    if (executor != null) {
+        executor.interrupt(Result.NOT_BUILT)
+        executor = null
+    } else {
+        echo "could not cancel an older flow because it has no assigned executor"
+    }
 }
